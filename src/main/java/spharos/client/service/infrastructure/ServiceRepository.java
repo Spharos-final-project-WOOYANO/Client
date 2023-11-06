ee490d8e01f8c51b057f42ddef5f7e1704ada5ca
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-11-06 15:06:08 +0900

    [edit] JenkinsFile-secretFile + serviceFile annotation

--- src/main/java/spharos/client/service/infrastructure/ServiceRepository.java
+++ src/main/java/spharos/client/service/infrastructure/ServiceRepository.java
@@ -3,18 +3,18 @@ package spharos.client.service.infrastructure;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import spharos.client.service.domain.QService;
-import spharos.client.service.dto.SearchServiceDto;
-
-import java.util.List;
+//import spharos.client.service.domain.QService;
+//import spharos.client.service.dto.SearchServiceDto;
+//
+//import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class ServiceRepository implements JPAServicecRepository {
 
-    private final JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+//    private final JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
 
-    QService s = QService.service;
-    List<SearchServiceDto>
+//    QService s = QService.service;
+//    List<SearchServiceDto>
 
 }
