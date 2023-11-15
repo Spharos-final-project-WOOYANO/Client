7ee99650cea33231368da71543b12ebf41a1dd67
Your Name <you@example.com>
2023-11-15 20:15:05 +0900

    [update] 에러나는 코드 주석처리

--- src/main/java/spharos/client/service/application/SearchService.java
+++ src/main/java/spharos/client/service/application/SearchService.java
@@ -3,6 +3,5 @@ package spharos.client.service.application;
 import java.time.LocalDate;
 
 public interface SearchService {
-    void findServiceList(String type);
     void findSearchResult(String type, LocalDate date, int region);
 }
--- src/main/java/spharos/client/service/application/SearchServicelmpl.java
+++ src/main/java/spharos/client/service/application/SearchServicelmpl.java
@@ -7,8 +7,6 @@ import spharos.client.service.domain.category.converter.BaseTypeConverter;
 import spharos.client.service.domain.category.enumType.ServiceBaseCategoryType;
 import spharos.client.service.infrastructure.ServiceAreaRepository;
 import spharos.client.service.infrastructure.ServiceCategoryRepository;
-import spharos.client.service.infrastructure.SearchRepository;
-
 import java.time.LocalDate;
 import java.util.List;
 
@@ -19,24 +17,23 @@ public class SearchServicelmpl implements SearchService{
 
     private final ServiceCategoryRepository serviceCategoryRepository;
     private final ServiceAreaRepository serviceAreaRepository;
-    private final SearchRepository searchRepository;
-    @Override
-    public void findServiceList(String type){
 
-//        ServiceBaseCategoryType resultType = new BaseTypeConverter().convertToEntityAttribute(type);
-//        List<Long> serviceList = serviceCategoryRepository.findCategoryIdEqualServiceId(resultType);
-    }
     @Override
     public void findSearchResult(String type, LocalDate date, int region) {
 
-        LocalDate now = LocalDate.now();//date가 null일 경우 현재 날짜를 기준으로 검색
+//        LocalDate now = LocalDate.now();//date가 null일 경우 현재 날짜를 기준으로 검색
 
         //↓ 1. 프론트에서 받아온 지역코드를 통해 해당 지역에 서비스를 제공하는 업체를 검색
-        List<Long> listOfProvideServicesToRegion = serviceAreaRepository.findServiceIdByAreaCodeIn(region);
+//        List<Long> listOfProvideServicesToRegion = serviceAreaRepository.findServiceIdByAreaCodeIn(region);
 
         //↓ 2. 업체 카테고리로 다시 serviceIdList를 필터링한다.
-        ServiceBaseCategoryType serviceType = new BaseTypeConverter().convertToEntityAttribute(type);
-        List <Long> serviceIdList = serviceCategoryRepository.findByServiceIdInAndCategoryBaseCategory(serviceType,listOfProvideServicesToRegion);
+//        ServiceBaseCategoryType serviceType = new BaseTypeConverter().convertToEntityAttribute(type);
+//        for (Long id:
+//             listOfProvideServicesToRegion) {
+//
+//            id = serviceCategoryRepository.findByServiceIdInCategoryBaseCategory(serviceType,id);
+//        }
+//        List <Long> serviceIdList = serviceCategoryRepository.findByServiceIdInCategoryBaseCategory(serviceType,listOfProvideServicesToRegion);
 
         //↓ 3. 검색한 serviceIdList를 통해 해당 날짜에 서비스가능한 업체를 검색한다.
 
