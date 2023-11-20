12b64759ce526c445b1a27587c66220c5580109e
ASY <showasy@naver.com>
2023-11-20 17:42:13 +0900

    Feat 리뷰의 서비스명과 기사명 조회 기능 추가

--- src/main/java/spharos/client/global/common/response/ResponseCode.java
+++ src/main/java/spharos/client/global/common/response/ResponseCode.java
@@ -22,7 +22,8 @@ public enum ResponseCode {
     WITHDRAW_CLIENT(HttpStatus.BAD_REQUEST, false, 2040,"탈퇴한 회원입니다."),
     DORMANT_CLIENT(HttpStatus.BAD_REQUEST, false, 2050,"휴면 회원입니다."),
     WAIT_CLIENT(HttpStatus.BAD_REQUEST, false, 2060,"입점 대기중인 회원입니다."),
-    CANNOT_FIND_SERVICE(HttpStatus.BAD_REQUEST, false, 2070,"서비스 정보를 찾을 수 없습니다.");
+    CANNOT_FIND_SERVICE(HttpStatus.BAD_REQUEST, false, 2070,"서비스 정보를 찾을 수 없습니다."),
+    CANNOT_FIND_WORKER(HttpStatus.BAD_REQUEST, false, 2080,"기사 정보를 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
---
+++ src/main/java/spharos/client/service/vo/request/ServiceOperateRegisterRequest.java
@@ -0,0 +1,11 @@
+package spharos.client.service.vo.request;
+
+import lombok.Getter;
+
+@Getter
+public class ServiceOperateRegisterRequest {
+
+    // 운영시간 리스트(요일, 오픈시간, 마감시간)
+    // 휴무일 리스트(주기, 요일)
+
+}
