171e683113c848d8833b969290810957195ac58f
Go Shin Young <118165375+0-shingo@users.noreply.github.com>
2023-11-24 20:26:30 +0900

    Update SearchService.java

--- src/main/java/spharos/client/service/application/SearchService.java
+++ src/main/java/spharos/client/service/application/SearchService.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface SearchService {
     List<Long> findServiceList(String type, LocalDate date, Integer region) throws ParseException;
 
-    List<SearchServiceDataListResponse> findServiceListData(List<Long> serviceIdList);
+    List<SearchServiceDataListResponse> findServiceListData(List<Long> serviceIdList,String type);
 }
