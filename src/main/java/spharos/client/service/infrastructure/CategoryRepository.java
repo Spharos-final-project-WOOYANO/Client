54073ba65c28445ae84981a4bf7c8f7bf2a0e40c
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-11-28 15:13:30 +0900

    [FIX] 업체 검색기능 - 수정7

--- src/main/java/spharos/client/service/infrastructure/CategoryRepository.java
+++ src/main/java/spharos/client/service/infrastructure/CategoryRepository.java
@@ -2,12 +2,10 @@ package spharos.client.service.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import spharos.client.service.domain.category.Category;
-import spharos.client.service.domain.category.enumType.ServiceBaseCategoryType;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Optional<Category> findByBaseCategory(ServiceBaseCategoryType type);
 
     Optional<Category> findById(Long id);
 }
