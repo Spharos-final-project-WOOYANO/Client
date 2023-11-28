ae0cb8b8abbe58426cec1f084c84e3c17d58f2ef
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-11-28 17:24:57 +0900

    [FIX] 업체 검색기능 - 수정 11

--- src/main/java/spharos/client/worker/infrastructure/WorkerReservationHistoryRepository.java
+++ src/main/java/spharos/client/worker/infrastructure/WorkerReservationHistoryRepository.java
@@ -1,6 +1,8 @@
 package spharos.client.worker.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spharos.client.worker.domain.WorkerReservationHistory;
 import java.time.LocalDate;
 import java.util.List;
@@ -12,6 +14,7 @@ public interface WorkerReservationHistoryRepository extends JpaRepository<Worker
 
     Optional<WorkerReservationHistory> findByReservationNum(String reservationNum);
 
-    List<WorkerReservationHistory> findByWorkerIdAndReservationDate(Long workerId, LocalDate date);
+    @Query("SELECT wrh FROM WorkerReservationHistory wrh JOIN wrh.worker w WHERE w.id = :workerId AND wrh.reservationDate = :date")
+    List<WorkerReservationHistory> findByWorkerIdAndReservationDate(@Param("workerId") Long workerId, @Param("date") LocalDate date);
 
 }
